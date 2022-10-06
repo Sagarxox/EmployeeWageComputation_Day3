790e538cb0d012d46f0e5a2eb90508a2e8ce17e0
sagar <skatahre@gmail.com>
2022-10-06 14:54:35 +0530

    Assuming 20 Working Day per Month

---
+++ EmployeeWage_UC5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    public class EmployeeCheck
+    {
+        public static void EmployeeAttendence()
+        {
+
+            const int Is_Full_time = 1;
+            const int Is_Part_time = 2;
+            const int Emp_Rate_per_hour = 20;
+            //Variables
+            int Num_Of_Working_days = 20;
+            int totalHrs = 0;
+            int empHrs = 0;
+            int empWage = 0;
+            for (int i = 0; i < Num_Of_Working_days; i++)
+            {
+                Random obj = new Random();
+                int empCheck = obj.Next(3);
+
+                //Computation
+                switch (empCheck)
+                {
+                    case Is_Full_time:
+                        empHrs = 8;
+                        break;
+
+                    case Is_Part_time:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalHrs += empHrs;
+            }
+            
+
+            Console.WriteLine("Total working hours: {0}", totalHrs);
+            empWage = Emp_Rate_per_hour * totalHrs;
+            Console.WriteLine("Total employee wage is: " + empWage);
+
+
+
+        }
+    }
+}
